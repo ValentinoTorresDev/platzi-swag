49349e271dcd23b4e96cd9c430acbabde140d6e9
ValentinoT <danieltorresc13@gmail.com>
2020-09-04 07:06:59 -0500

    gatsby plugin manisfest eliminado

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,53 +1,53 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
 })
 
 module.exports = {
   siteMetadata: {
-    title: `PlatziSwag`,
-    description: `El mejor swag de platzi.`,
-    author: `Valentino Torres`,
+    title: 'PlatziSwag',
+    description: 'El mejor swag de platzi.',
+    author: 'Valentino Torres'
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-stripe`,
-    `gatsby-plugin-styled-components`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-stripe',
+    'gatsby-plugin-styled-components',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        name: 'images',
+        path: `${__dirname}/src/images`
+      }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-typography',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        pathToConfigModule: './src/utils/typography.js'
+      }
     },
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: 'gatsby-source-stripe',
       options: {
-        pathToConfigModule: "./src/utils/typography.js",
-      },
-    },
-    {
-      resolve: `gatsby-source-stripe`,
-      options: {
-        objects: ["Sku"],
-        secretKey: process.env.STRIPE_SK,
-      },
-    },
+        objects: ['Sku'],
+        secretKey: process.env.STRIPE_SK
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  ]
 }
